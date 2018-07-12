4f48f3b6ce765d99d8211e7acda8004b41babbde
deva07 <41105274+deva07@users.noreply.github.com>
2018-07-12 09:37:21 +0530

    Create dd.java

---
+++ dd.java
@@ -0,0 +1,13 @@
+package activity;
+
+import java.util.Scanner;
+
+public class Addition {
+	public static void main(String[] args){
+		String name="Deva priyan";
+		int n=name.length();
+		char ch=name.charAt(n-1);
+		System.out.println(ch);
+	}
+
+}
